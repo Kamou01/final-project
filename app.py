25b58f3d5bb509e7a7f946fefc80c704b38f33cb
Kamou01 <kamogelomkonto01@gmail.com>
2021-08-26 08:26:03 +0200

    Update

---
+++ app.py
@@ -0,0 +1,242 @@
+import hmac
+import sqlite3
+
+from flask import Flask, request
+from flask_jwt import JWT, jwt_required, current_identity
+
+
+class userInfo(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+
+def fetch_users():
+    with sqlite3.connect('comicbook_store.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user")
+        users = cursor.fetchall()
+
+        new_data = []
+
+        for data in users:
+            new_data.append(userInfo(data[0], data[3], data[4]))
+    return new_data
+
+
+def init_usertable():
+    conn = sqlite3.connect('comicbook_store.db')
+    print("Opened database successfully")
+
+    conn.execute("CREATE TABLE IF NOT EXISTS user(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                 "first_name TEXT NOT NULL,"
+                 "last_name TEXT NOT NULL,"
+                 "username TEXT NOT NULL,"
+                 "password TEXT NOT NULL)")
+
+    conn.commit()
+    print("user table created successfully.")
+
+
+init_usertable()
+users = fetch_users()
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and hmac.compare_digest(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return username_table.get(user_id, None)
+
+
+def comicbook_table():
+    with sqlite3.connect('comicbook_store.db') as conn:
+        conn.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                     "name TEXT NOT NULL,"
+                     "price TEXT NOT NULL,"
+                     "description TEXT NOT NULL,"
+                     "category TEXT NOT NULL)")
+        print("comicbook table create successfully.")
+
+
+init_usertable()
+comicbook_table()
+
+users = fetch_users()
+
+
+app = Flask(__name__)
+app.debug = True
+app.config["SECRET_KEY"] = 'super-secret'
+
+jwt = JWT(app, authenticate, identity)
+
+
+@app.route('/protected')
+@jwt_required
+def protected():
+    return '%s' % current_identity
+
+
+@app.route('/user-registration/', methods=["POST"])
+def user_registration():
+    response = {}
+
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        username = request.form['username']
+        password = request.form['password']
+
+        print(first_name, username, password, last_name)
+
+        with sqlite3.connect('comicbook_store.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO user("
+                           "first_name,"
+                           "last_name,"
+                           "username,"
+                           "password) VALUES(?, ?, ?, ?)", (first_name, last_name, username, password))
+            conn.commit()
+            response["message"] = "success"
+            response["status_code"] = 201
+
+    return response
+
+
+@app.route('/addingProduct/', methods=["POST"])
+def add_comicbooks():
+    response = {}
+
+    if request.method == "POST":
+        name = request.form['name']
+        price = request.form['price']
+        description = request.form['description']
+        type = request.form['type']
+
+        with sqlite3.connect('comicbook_store.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO comicbooks("
+                               "name,"
+                               "price,"
+                               "description,"
+                               "category) VALUES(?, ?, ?, ?)", (name, price, description, type))
+                conn.commit()
+                response["status_code"] = 201
+                response['description'] = "Product added successfully"
+        return response
+
+
+@app.route('/delete/<int:id>/')
+def delete_products(product_id):
+    response = {}
+
+    with sqlite3.connect('comicbook_store.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM comicsbooks WHERE id=" + str(product_id))
+        connection.commit()
+        response['status code'] = 200
+        response['message'] = "Book deleted."
+    return response
+
+
+@app.route('/view_products/')
+def view_products():
+    response = {}
+
+    with sqlite3.connect('comicbook_store.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM comicbooks')
+        conn.commit()
+
+        products = cursor.fetchall()
+        response["status_code"] = 201
+        response["products"] = products
+        response['description'] = "Here are the comics"
+
+    return response
+
+
+@app.route('/updating_product/', methods=["PUT"])
+def update_comics(id):
+    response = {}
+
+    if request.method == "PUT":
+        with sqlite3.connect('comicbook_store.db') as conn:
+            print(request.json)
+            incoming_data = dict(request.json)
+            put_data = {}
+
+            if incoming_data.get("name") is not None:
+                put_data["name"] = incoming_data.get("name")
+
+                with sqlite3.connect('comicbook_store.db') as connection:
+                    cursor = connection.cursor()
+                    cursor.execute("UPDATE comicbooks SET name =? WHERE id", (put_data["name"], id))
+
+                    conn.commit()
+                    response['message'] = "Update was successful"
+                    response['status_code'] = 200
+
+            elif incoming_data.get("price") is not None:
+                put_data["price"] = incoming_data.get("price")
+
+                with sqlite3.connect('online_store.db') as connection:
+                    cursor = connection.cursor()
+                    cursor.execute("UPDATE comicbook SET price =? WHERE id", (put_data["price"], id))
+
+                    conn.commit()
+                    response['message'] = "Update was successful"
+                    response['status_code'] = 200
+
+            elif incoming_data.get("description") is not None:
+                put_data["description"] = incoming_data.get("description")
+
+                with sqlite3.connect('comicbook_store.db') as connection:
+                    cursor = connection.cursor()
+                    cursor.execute("UPDATE comicbook SET description =? WHERE id", (put_data["description"], id))
+
+                    conn.commit()
+                    response['message'] = "Update was successful"
+                    response['status_code'] = 200
+
+            elif incoming_data.get("category") is not None:
+                put_data["category"] = incoming_data.get("category")
+
+                with sqlite3.connect('comicbook_store.db') as connection:
+                    cursor = connection.cursor()
+                    cursor.execute("UPDATE comicbook SET type =? WHERE id", (put_data["type"], id))
+                    conn.commit()
+                    response['message'] = "Update was successful"
+                    response['status_code'] = 200
+
+    return response
+
+
+@app.route('/view_product/<int:id>')
+def view_product(id):
+    response = {}
+
+    with sqlite3.connect('comicbook_store.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM comicbooks WHERE id =? ', str(id))
+        product = cursor.fetchone()
+        conn.commit()
+
+        response['status code'] = 201
+        response['description'] = "Here are the comics"
+        response['data'] = product
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
